767f71de3154be0cc8c37221ba2b60d6aa9c47bb
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-05-09 20:08:55 +0200

    Modify Program.cs

--- AddingBlazorToMVC.MVC/Program.cs
+++ AddingBlazorToMVC.MVC/Program.cs
@@ -1,7 +1,12 @@
+using AddingBlazorToMVC.MVC.Components;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
 
 var app = builder.Build();
 
@@ -19,9 +24,13 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseAntiforgery();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
